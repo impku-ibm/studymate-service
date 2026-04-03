9bdec375af98a7f953eee0c70400f1955dda4672
Pankajk <pankajk@unifocus.com>
2026-04-03 16:36:15 +0530

    fix: Revert Redis changes, keep Redis enabled with Upstash URL

--- src/main/java/com/portal/studymate/config/RedisConfiguration.java
+++ src/main/java/com/portal/studymate/config/RedisConfiguration.java
@@ -1,6 +1,5 @@
 package com.portal.studymate.config;
 
-import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -14,14 +13,11 @@ public class RedisConfiguration {
    public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
       RedisTemplate<String, String> template = new RedisTemplate<>();
       template.setConnectionFactory(factory);
-
       template.setKeySerializer(new StringRedisSerializer());
       template.setValueSerializer(new StringRedisSerializer());
       template.setHashKeySerializer(new StringRedisSerializer());
       template.setHashValueSerializer(new StringRedisSerializer());
-
       template.afterPropertiesSet();
       return template;
    }
-
 }
